9271122970b74e708737e8f00a50e4953b96d230
Nadim-Muhammad <nadim.muhammad99@gmail.com>
2024-01-19 12:29:25 +0100

    Added simulator.h and simulator.cpp. Tried to run nix project with it, but failed.

---
+++ src/simulator.cpp
@@ -0,0 +1,41 @@
+#include "simulator.h"
+#include <iostream>
+#include <vector>
+#include <random>
+#include <cmath>
+
+StockSimulator::StockSimulator(double iterations, double initialPrice, double years, double numDays, double timeStep, double mu, double sigma)
+    : iter(iterations), S_0(initialPrice), T(years), days(numDays), timestep(timeStep), mu(mu), sigma(sigma),
+      prices(iterations, std::vector<double>(numDays, 0.0)) {}
+
+void StockSimulator::runSimulation() {
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::normal_distribution<double> distribution(0.0, 1.0);
+
+    for (int i = 0; i < iter; ++i) {
+        double temp = S_0;
+
+        for (int j = 0; j < T * days; ++j) {
+            for (int k = 0; k < 1 / timestep; ++k) {
+                double dW = distribution(generator);
+                double change = timestep * mu + sigma * temp * sqrt(timestep) * dW;
+                temp = temp + change;
+            }
+            prices[i][j] = temp;
+        }
+    }
+}
+
+const std::vector<std::vector<double>>& StockSimulator::getPrices() const {
+    return prices;
+}
+
+void StockSimulator::printPrices() const {
+    for (const auto& iteration : prices) {
+        for (double price : iteration) {
+            std::cout << price << " ";
+        }
+        std::cout << std::endl;
+    }
+}
---
+++ src/simulator.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <random>
+#include <cmath>
+
+class StockSimulator {
+private:
+    double iter;
+    double S_0;
+    double T;
+    double days;
+    double timestep;
+    double mu;
+    double sigma;
+    std::vector<std::vector<double>> prices;
+
+public:
+    StockSimulator(double iterations, double initialPrice, double years, double numDays, double timeStep, double mu, double sigma);
+
+    void runSimulation();
+
+    const std::vector<std::vector<double>>& getPrices() const;
+
+    void printPrices() const;
+};
