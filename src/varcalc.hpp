044bf9769a48845e0844305ef9cb9a8cc62a6ba0
Staneesh <blkreasons@outlook.com>
2024-01-22 20:44:47 +0100

    Outline of a general algorithm

--- src/varcalc.hpp
+++ src/varcalc.hpp
@@ -5,17 +5,21 @@
 struct PayoutProperties {
   double ev;
   double var;
+  double stdev;
 };
 
 class VarCalc {
 public:
+  VarCalc() = delete;
+  ~VarCalc(){}; // Don't free any memory - data is borrowed, outlives VarCalc!
+
   VarCalc(const std::vector<std::vector<double>> &p,
           const std::vector<OptionBase *> pf)
       : paths(p), portfolio(pf){};
 
-  double getPayout();
+  PayoutProperties getPayoff();
 
 private:
   std::vector<OptionBase *> portfolio;
-  std::vector<std::vector<double>> paths;
+  const std::vector<std::vector<double>> &paths;
 };
